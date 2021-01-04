9db7239c877649cc17aed0d91c3e6c4c5d642287
Andrew Martynyshyn <andriy.martynyshyn@gmail.com>
2021-01-04 22:07:29 +0200

    anagrams solved. added better solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,6 +9,24 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
+
+module.exports = anagrams;
+
+/**
+ *
+ * first solution
+ *
+ */
+
+/*
+
+ function anagrams(stringA, stringB) {
   const exp = /[^\w]/g;
   stringA = stringA.replace(exp, "");
   stringB = stringB.replace(exp, "");
@@ -37,4 +55,4 @@ function anagrams(stringA, stringB) {
   return isAnagram;
 }
 
-module.exports = anagrams;
+ */
